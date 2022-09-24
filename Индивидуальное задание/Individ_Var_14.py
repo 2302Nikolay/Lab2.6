033afbec2ab56f0dd9a71c5024cecdbf2081dca6
2302Nikolay <Malzewn@gmail.com>
2022-09-24 09:22:30 +0300

    made ind task

--- Индивидуальное задание/Individ_Var_14.py
+++ Индивидуальное задание/Individ_Var_14.py
@@ -2,7 +2,97 @@
 # _*_ coding: utf-8 _*_
 
 import sys
-from datetime import date
 
 if __name__ == '__main__':
+    # Список .
+    manslist = []
 
+    # Организовать бесконечный цикл запроса команд.
+    while True:
+        # Запросить команду из терминала.
+        command = input(">>> ").lower()
+
+        # Выполнить действие в соответствие с командой.
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            # Запросить данные .
+            name = input("Имя:  ")
+            number = input("Номер телефона ")
+            date = input("Дата рождения  ")
+
+            # Создать словарь.
+            man = {
+                'name': name,
+                'number': number,
+                'date': date,
+            }
+
+            # Добавить словарь в список.
+            manslist.append(man)
+            # Отсортировать список в случае необходимости.
+            if len(manslist) > 1:
+                manslist.sort(key=lambda item: item.get('name', ''))
+
+        elif command == 'list':
+            # Заголовок таблицы.
+            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+                '-' * 4,
+                '-' * 30,
+                '-' * 20,
+                '-' * 20
+            )
+            print(line)
+            print(
+                '| {:^4} | {:^30} | {:^20} | {:^20} |'.format(
+                    "No",
+                    "Имя",
+                    "Номер телефона",
+                    "Дата рождения"
+                )
+            )
+            print(line)
+
+            # Вывести данные о человеке.
+            for idx, man in enumerate(manslist, 1):
+                print(
+                    '| {:>4} | {:<30} | {:<20} | {:>20} |'.format(
+                        idx,
+                        man.get('name', ''),
+                        man.get('number', ''),
+                        man.get('date', '')
+                    )
+                )
+
+            print(line)
+
+        elif command.startswith('select '):
+            parts = command.split(' ', maxsplit=1)
+            sel = (parts[1])
+
+            count = 0
+            for man in manslist:
+                if man.get('name') == sel:
+                    count += 1
+                    print(
+                        '{:>4}: {}'.format(count, man.get('name', ''))
+                    )
+                    print('Номер телефона:', man.get('number', ''))
+                    print('Дата рождения:', man.get('date', ''))
+
+            # Если счетчик равен 0, то человек не найден.
+            if count == 0:
+                print("Человек не найден.")
+
+        elif command == 'help':
+            # Вывести справку о работе с программой.
+            print("Список команд:\n")
+            print("add - добавить человека;")
+            print("list - вывести список людей;")
+            print("select <товар> - информация о человеке;")
+            print("help - отобразить справку;")
+            print("exit - завершить работу с программой.")
+
+        else:
+            print("Неизвестная команда {command}", file=sys.stderr)
